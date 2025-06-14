acd3ccf73996924cef8aa64433093d94d0e4d554
Deepjyoti-Sarmah <deepjyotisarmah37@gmail.com>
2025-06-14 13:37:31 +0530

    reading files

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use clap::Parser;
-use std::path::PathBuf;
+use owo_colors::OwoColorize;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+};
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = "Best ls command ever")]
@@ -9,5 +13,33 @@ struct Cli {
 
 fn main() {
     let cli = Cli::parse();
-    println!("Hello, world!");
+
+    let path = cli.path.unwrap_or(PathBuf::from("."));
+
+    if let Ok(does_exists) = fs::exists(&path) {
+        if does_exists {
+            for file in get_files(&path) {
+                println!("{}", file)
+            }
+        } else {
+            println!("{}", "Path does not exists".red());
+        }
+    } else {
+        println!("{}", "error reading directory".red());
+    }
+
+    // println!("{}", path.display());
+}
+
+fn get_files(path: &Path) -> Vec<String> {
+    let mut data = Vec::default();
+    if let Ok(read_dir) = fs::read_dir(path) {
+        for entry in read_dir {
+            if let Ok(file) = entry {
+                data.push(file.file_name().into_string().unwrap_or("unknown".into()));
+            }
+        }
+    }
+
+    data
 }
